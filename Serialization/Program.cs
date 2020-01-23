0cac43cab9cd90ca8327c71c4c13278fdec02daf
Ильнур Гиззатуллин <juvebars@mail.ru>
2020-01-23 12:54:54 +0300

    Добавьте файлы проекта.

---
+++ Serialization/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var groups = new List<Group>();
+            var students = new List<Student>();
+
+            for(int i = 1; i < 10; i++)
+            {
+                groups.Add(new Group(i, "Группа: " + i));
+            }
+
+            for(int i = 0; i < 300; i++)
+            {
+                var student = new Student(Guid.NewGuid().ToString().Substring(0,5), i % 100)
+                {
+                    Group = groups[i % 9]
+                };
+
+                students.Add(student);
+
+            }
+
+            var binFormatter = new BinaryFormatter();
+
+            using (var file = new FileStream("groups.bin", FileMode.OpenOrCreate))
+            {
+                binFormatter.Serialize(file, groups);
+            }
+
+            using (var file = new FileStream("groups.bin", FileMode.OpenOrCreate))
+            {
+                var newGroups = binFormatter.Deserialize(file) as List<Group>;
+
+                if(newGroups != null)
+                {
+                    foreach(var group in groups)
+                    {
+                        Console.WriteLine(group);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Serialization/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    class Student
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public Group Group { get; set; }
+        public Student(string name, int age)
+        {
+            //Check input arguments
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
